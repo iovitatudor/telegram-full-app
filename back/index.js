c0b7fb6c383e470c779ef52a5342cc9af4466997
Tudor Iovita <iovitatudor21@gmail.com>
2024-01-11 19:09:01 +0200

    Add time lottery

--- back/index.js
+++ back/index.js
@@ -5,23 +5,24 @@ const bot = new TelegramBot(token, {polling: true});
 const appUrl = "https://artcoin.media/";
 // const newApp = "https://cute-kataifi-2a18e8.netlify.app";
 const newApp = "https://app.artcoin.media?cache=1";
+const timeTG = "https://front.time.cheap";
 
 bot.on('message', async (msg) => {
     const chatId = msg.chat.id;
     const message = msg.text;
     if (message === "/start") {
-        // await bot.sendMessage(chatId, "Access our web app", {
-        //     reply_markup: {
-        //         inline_keyboard: [
-        //             [{text: 'open web app', web_app: {url: appUrl}}]
-        //         ],
-        //     }
-        // })
-
-        await bot.sendMessage(chatId, "ARTCOIN Telegram webApp", {
+        await bot.sendMessage(chatId, "TimeCoin Lottery", {
             reply_markup: {
                 inline_keyboard: [
-                    [{text: 'ARTCOIN Telegram webApp', web_app: {url: newApp}}]
+                    [{text: 'Go to TimeCoin Lottery', web_app: {url: timeTG}}]
+                ],
+            }
+        })
+
+        await bot.sendMessage(chatId, "ARTCOIN marketplace", {
+            reply_markup: {
+                inline_keyboard: [
+                    [{text: 'Go to ArtCoin marketplace', web_app: {url: newApp}}]
                 ],
             }
         })
