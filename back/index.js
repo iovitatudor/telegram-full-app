4a2463a406dc204d68b78cbe705ba5f7b43a5312
Tudor Iovita <iovitatudor21@gmail.com>
2024-01-12 18:45:04 +0200

    Change app url

--- back/index.js
+++ back/index.js
@@ -5,20 +5,11 @@ const bot = new TelegramBot(token, {polling: true});
 const appUrl = "https://artcoin.media/";
 // const newApp = "https://cute-kataifi-2a18e8.netlify.app";
 const newApp = "https://app.artcoin.media?cache=1";
-const timeTG = "https://front.time.cheap";
 
 bot.on('message', async (msg) => {
     const chatId = msg.chat.id;
     const message = msg.text;
     if (message === "/start") {
-        await bot.sendMessage(chatId, "TimeCoin Lottery", {
-            reply_markup: {
-                inline_keyboard: [
-                    [{text: 'Go to TimeCoin Lottery', web_app: {url: timeTG}}]
-                ],
-            }
-        })
-
         await bot.sendMessage(chatId, "ARTCOIN marketplace", {
             reply_markup: {
                 inline_keyboard: [
